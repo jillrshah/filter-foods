5eec85c8bc7ae89db1827331b02dec7f79c13812
Jill Shah <jillrshah@gmail.com>
2020-03-31 22:48:22 -0400

    js complete

---
+++ main.js
@@ -0,0 +1,6 @@
+$("body").on("change", ".filter", function (e) {
+	const this_select = e.target;
+	const choice = $(this_select).val();
+	$(".item.active").removeClass("active");
+	$(`.item.${choice}`).addClass("active");
+});
